c4f618fec48c28b907ee5bda3c513bd01f056434
Cedric Chee <cedric@invictusbyte.com>
2018-08-01 19:54:34 +0800

    New Jest config file

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "roots": [
+        "<rootDir>/src"
+    ],
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ],
+    "testEnvironment": "node",
+
+    // Setup Enzyme
+    "snapshotSerializers": ["enzyme-to-json/serializer"],
+    "setupTestFrameworkScriptFile": "<rootDir>/src/setupEnzyme.ts",
+}
