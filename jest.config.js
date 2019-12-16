5942dad713ee77d3df785de142aa1f1e3344bcef
Cedric Chee <cedric@invictusbyte.com>
2019-12-16 23:38:12 +0800

    Don't fail Jest on TS error

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,20 @@
 module.exports = {
-    "roots": [
-        "<rootDir>/src"
-    ],
-    "transform": {
-        "^.+\\.tsx?$": "ts-jest"
-    },
-    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    "moduleFileExtensions": [
-        "ts",
-        "tsx",
-        "js",
-        "jsx",
-        "json",
-        "node"
-    ],
-    "testEnvironment": "node",
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      diagnostics: {
+        warnOnly: true
+      }
+    }
+  },
 
-    // Setup Enzyme
-    "snapshotSerializers": ["enzyme-to-json/serializer"],
-    "setupTestFrameworkScriptFile": "<rootDir>/src/setupEnzyme.ts",
-}
+  // Setup Enzyme
+  snapshotSerializers: ["enzyme-to-json/serializer"],
+  setupTestFrameworkScriptFile: "<rootDir>/src/setupEnzyme.ts"
+};
